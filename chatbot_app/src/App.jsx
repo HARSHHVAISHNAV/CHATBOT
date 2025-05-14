82e0643fdc697c08038406f0a71079c7b228275c
HARSHHVAISHNAV <harshvaishnav6060@gmail.com>
2025-05-15 03:12:15 +0530

    hello

--- chatbot_app/src/App.jsx
+++ chatbot_app/src/App.jsx
@@ -15,7 +15,7 @@ function App() {
     setLoading(true);
 
     try {
-      const res = await fetch("http://localhost:5000/api/chat", {
+      const res = await fetch("https://chatbot-ycde.onrender.com/chat", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ message: input }),
